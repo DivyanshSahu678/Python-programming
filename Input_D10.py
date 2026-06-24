e8784c02c2b4e5fd3fdda5eb75a20ec113b56f93
DivyanshSahu678 <sahudivyansh965@gmail.com>
2026-06-24 21:03:56 +0530

    Taking Input from User

--- Input_D10.py
+++ Input_D10.py
@@ -2,8 +2,8 @@
 
 #taking input from the user using input function and storing it in a variable called name
 
-a = input("Enter your name: ")
-print("My name is " + a)
+b = input("Enter your name: ")
+print("My name is " + b)
 
 x = input("Enter first number: ")
 y = input("Enter second number: ")
